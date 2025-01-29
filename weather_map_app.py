729d3ca8df0605406d11a3a8736a797232eaf187
clarencejeddmaas <clarencemaas74@gmail.com>
2025-01-29 10:07:14 +0800

    initialize main app window

--- weather_map_app.py
+++ weather_map_app.py
@@ -13,3 +13,11 @@ import pytz
 from PIL import Image, ImageTk
 from timezonefinder import TimezoneFinder
 from tkintermapview import TkinterMapView
+
+# INITIALIZE THE MAIN APP WINDOW
+root = Tk()
+root.title("WeatherMap Explorer")
+root.geometry("890x470+300+300")
+root.configure(bg="#57adff")
+root.resizable(False, False)
+
