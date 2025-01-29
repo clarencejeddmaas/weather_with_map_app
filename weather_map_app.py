654665a8227d31dff9adf477cb64e1730009e711
clarencejeddmaas <clarencemaas74@gmail.com>
2025-01-29 10:03:43 +0800

    import libraries

---
+++ weather_map_app.py
@@ -0,0 +1,15 @@
+# INSTALL AND IMPORT REQUIRED LIBRARIES
+from tkinter import *
+import tkinter as tk
+from tkinter import PhotoImage, Button, Frame
+from geopy.geocoders import Nominatim
+from tkinter import messagebox
+from datetime import *
+import requests
+import re 
+import time
+import os
+import pytz
+from PIL import Image, ImageTk
+from timezonefinder import TimezoneFinder
+from tkintermapview import TkinterMapView
